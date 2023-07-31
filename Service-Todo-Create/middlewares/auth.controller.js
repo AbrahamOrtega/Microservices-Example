f3ade009f30ad74e9c449b9954b9f8dc0da780ce
Abraham Ortega <612010583@utsalamanca.edu.mx>
2023-07-31 13:36:27 -0600

    avance Todo-Create

---
+++ Service-Todo-Create/middlewares/auth.controller.js
@@ -0,0 +1,8 @@
+
+const tokenValidation = (req, res, next) => {
+    const token = req.headers["x-access-token"];
+
+    if(!token) return res.status(403).json({message: "No token provided"});
+
+    next();
+}
