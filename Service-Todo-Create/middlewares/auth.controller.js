373f17ceb9a7475bc0b49b8ee37cf39b5a70fccf
AbrahamOrtega <612010583@utsalamanca.edu.mx>
2023-07-31 16:33:51 -0600

    create middleware

--- Service-Todo-Create/middlewares/auth.controller.js
+++
@@ -1,8 +0,0 @@
-
-const tokenValidation = (req, res, next) => {
-    const token = req.headers["x-access-token"];
-
-    if(!token) return res.status(403).json({message: "No token provided"});
-
-    next();
-}
